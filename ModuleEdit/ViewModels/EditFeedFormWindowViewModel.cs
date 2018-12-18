0abd8fe8a7b910264c6bc4742dc660bf7ac6f923
FabianPerl_Windows <fabian.perl@hof-university.de>
2018-12-18 20:14:15 +0100

    working on the edit window

--- ModuleEdit/ViewModels/EditFeedFormWindowViewModel.cs
+++ ModuleEdit/ViewModels/EditFeedFormWindowViewModel.cs
@@ -19,7 +19,7 @@ namespace ModuleEdit.ViewModels
         private readonly ILoggerFacade _logger = ProjectLogger.GetLogger;
         private readonly IEventAggregator _eventAggregator;
         private readonly IRssStore _rssStore;
-        private Source _soureToEdit;
+        private Source _sourceToEdit;
 
         public EditFeedFormWindowViewModel(IEventAggregator eventAggregator, IRssStore rssStore)
         {
@@ -27,23 +27,62 @@ namespace ModuleEdit.ViewModels
             _rssStore = rssStore;
             AllSources = _rssStore.GetAllSources();
             RemoveSourceCommand = new DelegateCommand<Source>(RemoveOneSource);
-            EditSourceCommand = new DelegateCommand<Source>(EditOneSource);
+            EditSourceCommand = new DelegateCommand(EditOneSource);
+            PreviewEditSourceCommand = new DelegateCommand<Source>(PreviewOneSource);
+
+            if(AllSources.Count >= 1)
+                SourceToEdit = AllSources.ElementAt(0);
         }
 
         public ICollection<Source> AllSources { get; }
-        public DelegateCommand<Source> RemoveSourceCommand;
-        public DelegateCommand<Source> EditSourceCommand;
+        public DelegateCommand<Source> RemoveSourceCommand { get; }
+        public DelegateCommand EditSourceCommand { get; }
+        public DelegateCommand<Source> PreviewEditSourceCommand { get; }
+
+        public string NameOfSource
+        {
+            get => _sourceToEdit?.Name;
+            set
+            {
+                if (_sourceToEdit == null) return;
+                _sourceToEdit.Name = value;
+            }
+        }
+
+        public Uri UriOfSource
+        {
+            get => _sourceToEdit?.FeedUri;
+            set
+            {
+                if (_sourceToEdit == null) return;
+                _sourceToEdit.FeedUri = value;
+            }
+        }
+
+        public string CategoryOfSource
+        {
+            get => _sourceToEdit?.Category;
+            set
+            {
+                if (_sourceToEdit == null) return;
+                _sourceToEdit.Category = value;
+            }
+        }
 
         public Source SourceToEdit
         {
-            get => _soureToEdit;
-            set => SetProperty(ref _soureToEdit, value);
+            get => _sourceToEdit;
+            set => SetProperty(ref _sourceToEdit, value);
         }
 
         #region helper
-        private void EditOneSource(Source source)
+        private void EditOneSource()
         {
 
+        }
+        private void PreviewOneSource(Source source)
+        {
+            SourceToEdit = source;
         }
 
         private void RemoveOneSource(Source source)

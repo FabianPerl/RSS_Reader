0da2e9277d4af1ebe72b91bcbc151692d369bee7
FabianPerl_Windows <fabian.perl@hof-university.de>
2019-01-12 13:23:00 +0100

    add doc to editing window

--- ModuleEdit/ViewModels/EditFeedFormWindowViewModel.cs
+++ ModuleEdit/ViewModels/EditFeedFormWindowViewModel.cs
@@ -12,6 +12,9 @@ using Prism.Logging;
 
 namespace ModuleEdit.ViewModels
 {
+    /// <summary>
+    /// Viewmodel for the window edit feed
+    /// </summary>
     public class EditFeedFormWindowViewModel : BindableBase
     {
         private readonly ILoggerFacade _logger = ProjectLogger.GetLogger;
@@ -31,16 +34,36 @@ namespace ModuleEdit.ViewModels
         }
 
         #region delegates
+        /// <summary>
+        /// Handles the interaction that the user wants to remove the clicked source
+        /// </summary>
         public DelegateCommand RemoveSourceCommand { get; }
+
+        /// <summary>
+        /// Handles the interaction that the user wants to edit the clicked source
+        /// </summary>
         public DelegateCommand EditSourceCommand { get; }
+
+        /// <summary>
+        /// Handles the interaction that the user wants to see the saved data from the clicked source
+        /// </summary>
         public DelegateCommand<Source> PreviewEditSourceCommand { get; }
         #endregion
 
         #region attributes
+        /// <summary>
+        /// Gets all available categories for the Source
+        /// </summary>
         public IEnumerable<Categories> Categories { get; } = Enum.GetValues(typeof(Categories)).Cast<Categories>();
 
+        /// <summary>
+        /// Gets all Sources saved from the user
+        /// </summary>
         public ICollection<Source> AllSources { get; }
 
+        /// <summary>
+        /// Gets and Sets the name of the clicked Source
+        /// </summary>
         private string _name;
         public string NameOfSource
         {
@@ -48,6 +71,9 @@ namespace ModuleEdit.ViewModels
             set => SetProperty(ref _name, value);
         }
 
+        /// <summary>
+        /// Gets and Sets the uri of the clicked Source
+        /// </summary>
         private Uri _uri;
         public Uri UriOfSource
         {
@@ -55,6 +81,9 @@ namespace ModuleEdit.ViewModels
             set => SetProperty(ref _uri, value);
         }
 
+        /// <summary>
+        /// Gets and Sets the category of the clicked Source
+        /// </summary>
         private string _category;
         public string CategoryOfSource
         {
@@ -62,6 +91,9 @@ namespace ModuleEdit.ViewModels
             set => SetProperty(ref _category, value);
         }
 
+        /// <summary>
+        /// Gets and Sets the Source which should be shown in the preview
+        /// </summary>
         public Source SourceToEdit
         {
             get => _sourceToEdit;
@@ -77,6 +109,9 @@ namespace ModuleEdit.ViewModels
         #endregion
 
         #region helper
+        /// <summary>
+        /// Initializes variables with the edited source and publishes an event that the source was edited
+        /// </summary>
         private void EditOneSource()
         {
             _sourceToEdit.FeedUri = UriOfSource;
@@ -85,12 +120,19 @@ namespace ModuleEdit.ViewModels
             _eventAggregator.GetEvent<EditSourceEvent>().Publish(_sourceToEdit);
         }
         
+        /// <summary>
+        /// Shows the data saved from the source
+        /// </summary>
+        /// <param name="source">The source that should be shown</param>
         private void PreviewOneSource(Source source)
         {
             _logger.Log("Preview " + source.Name + " with URI " + source.FeedUri + " and ID: " + source.Id, Category.Info, Priority.Medium);   
             SourceToEdit = source;
         }
 
+        /// <summary>
+        ///  Publishes an event with the source that should be removed
+        /// </summary>
         private void RemoveOneSource()
         {
             _logger.Log("Remove " + _sourceToEdit.Name + " with URI " + _sourceToEdit.FeedUri + " and ID: " + _sourceToEdit.Id, Category.Info, Priority.Medium);   
